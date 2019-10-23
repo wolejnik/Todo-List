cf01154fa4ff4d9408348c85907ff6dd725d5843
Wojtek <olejnik.wojciech1@gmail.com>
2019-10-24 00:18:47 +0200

    Created possibility to show todo task list

--- Todo-List-app/src/app/todo-list/todo-list.component.ts
+++ Todo-List-app/src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { TaskListService } from '../shared/task-list/task-list.service';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  todoTasks: Task[];
+
+  constructor(
+    private taskService: TaskListService,
+    private fireAuth: AngularFireAuth
+  ) {
+    this.fireAuth.authState.subscribe(user => {
+      this.user = user;
+    });
+   }
 
   ngOnInit() {
+    this.init();
+   }
+
+   async init() {
+    await this.taskService.getTasks().subscribe(t => {
+      this.todoTasks = t.map(item => {
+        return {
+          taskID: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as Task;
+      });
+    });
+   }
+
+   onDelete(taskID: string) {
+     this.taskService.deleteTask(taskID);
+   }
+
+   updateTaskToDone(taskID: string) {
+     this.taskService.updateTaskToDone(taskID);
+   }
+
+   updateTaskToProgress(taskID: string) {
+    this.taskService.updateTaskToProgress(taskID);
   }
 
 }
