d80d8b15f3dd1fd828fd229f7c2e226ce84b8fde
Wojtek <olejnik.wojciech1@gmail.com>
2019-10-23 23:59:00 +0200

    Added possibility login and register

--- Todo-List-app/src/app/home/home.component.ts
+++ Todo-List-app/src/app/home/home.component.ts
@@ -17,4 +17,8 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/login']);
  }
 
+ register() {
+  this.router.navigate(['/register']);
+}
+
 }
--- Todo-List-app/src/app/register/register.component.ts
+++ Todo-List-app/src/app/register/register.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from './../models/account';
+import { User } from 'firebase';
+import { AuthentificationService } from '../shared/auth/authentification.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-login',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  credentials: Account = {
+      login: '',
+      password: ''
+  };
+  user: User;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authService: AuthentificationService
+  ) { }
+
+  ngOnInit() { }
+
+  register() {
+    this.authService.register(this.credentials)
+    .then( () => {
+      console.log('Created account');
+      this.router.navigate(['/login']);
+    })
+    .catch(
+      error => console.log(error.message)
+    );
   }
 
 }
