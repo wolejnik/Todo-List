29f9b966cb30af8a5c6f8f2e4651b9e33f54ef6d
Wojtek <olejnik.wojciech1@gmail.com>
2019-10-24 23:59:55 +0200

    Created view for login

--- Todo-List-app/src/app/login/login.component.ts
+++ Todo-List-app/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Account } from './../models/account';
 import { User } from 'firebase';
 import { AuthentificationService } from '../shared/auth/authentification.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +17,23 @@ export class LoginComponent implements OnInit {
       password: ''
   };
   user: User;
+  formGroup: FormGroup;
+  titleAlert = 'To pole jest wymagane';
+  post: any = '';
+
+  // tslint:disable-next-line: max-line-length
+  emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(
     private router: Router,
-    private authService: AuthentificationService
-  ) { }
+    private authService: AuthentificationService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+     email : [null, [Validators.required, Validators.pattern(this.emailregex)]],
+      password : [null, [Validators.required, this.checkPassword]]
+    });
+   }
 
   ngOnInit() { }
 
@@ -35,4 +48,21 @@ export class LoginComponent implements OnInit {
     );
   }
 
+  checkPassword(control) {
+    const enteredPassword = control.value;
+    const passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
+  }
+
+  getErrorEmail() {
+    return this.formGroup.get('email').hasError('required') ? 'Pole jest wymagane' :
+      this.formGroup.get('email').hasError('pattern') ? 'Niepoprawny adres email' : '';
+  }
+
+  getErrorPassword() {
+    // tslint:disable-next-line: max-line-length
+    return this.formGroup.get('password').hasError('required') ? 'Pole jest wymagane (minimum 8 znaków, przynajmniej jedna wielka i mała litera)' :
+      // tslint:disable-next-line: max-line-length
+      this.formGroup.get('password').hasError('requirements') ? 'Hasło musi składać się z co najmniej ośmiu znaków, jednej dużej litery i jednej cyfry.' : '';
+  }
 }
