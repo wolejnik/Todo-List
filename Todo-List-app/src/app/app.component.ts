5bc2bed745dafb972de327fbe0eaca829d4e5700
wolejnik <olejnik.wojciech1@gmail.com>
2019-11-16 01:18:04 +0100

    Created icon to logout

--- Todo-List-app/src/app/app.component.ts
+++ Todo-List-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthentificationService } from './shared/auth/authentification.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo-List-app';
+
+  constructor(
+    private authService: AuthentificationService
+  ) {}
+
+  logout() {
+    this.authService.logout();
+  }
+
+
 }
